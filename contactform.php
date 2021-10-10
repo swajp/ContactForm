8eb1d8c2b85e91a2a2df5beec741c67acc962f0e
Mira Stejskal <65676574+swajp@users.noreply.github.com>
2021-10-10 15:57:54 +0200

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST['submit'])){
+   $name = $_POST['name']; 
+   $subject = $_POST['subject']; 
+   $mailFrom = $_POST['mail']; 
+   $message = $_POST['message']; 
+
+   $mailTo = "sondikemail@seznam.cz";
+   $headers = "From:" . $mailFrom;
+   $txt = "You have received an email from:" . $name . $message;
+
+   mail($mail,$subject, $txt, $headers);
+   head("Location: index.php?mailsend");
+}
