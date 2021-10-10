a8bb9e97adf09cbda9aa58ebbb2afafefe65e9ac
Mira Stejskal <65676574+swajp@users.noreply.github.com>
2021-10-10 15:56:00 +0200

    Add files via upload

---
+++ phpcourse/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form class="contact-form" action="contactform.php" method="post">
+        <input type="text" name="name" placeholder="Name">
+        <input type="text" name="mail" placeholder="Email">
+        <input type="text" name="subject" placeholder="Subject">
+        <textarea name="message" placeholder="Message"></textarea>
+        <button type="submit" name="submit">send mail</button>
+    </form>
+</body>
+</html>
